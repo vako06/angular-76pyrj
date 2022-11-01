00c480bd0b3ce1e101da1bfa3505d5d72c21b849
vako06 <116735988+vako06@users.noreply.github.com>
2022-11-01 11:14:40 +0400

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrls: ['./product-alerts.component.css'],
 })
 export class ProductAlertsComponent {
-
-  @Input() product!: Product;
-
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 }
